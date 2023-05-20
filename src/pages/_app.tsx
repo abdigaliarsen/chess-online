bb881c9a6c7f6c6f9535d01a08ad7fc741230961
quadropunk <abdigal.2015@gmail.com>
2023-05-20 15:02:37 +0600

    added footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,8 +18,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <title>Chess Online</title>
       <meta name="description" content="Chess Online by abdigaliarsen" />
       <link rel="icon" href="/favicon.ico" />
-      <div className="overflow-hidden">
-        <div className="min-h-screen bg-cover bg-center bg-repeat" style={{ backgroundImage: `url(${image.src})` }}>
+      <div className="overflow-hidden" style={{ backgroundImage: `url(${image.src})` }}>
+        <div className="min-h-screen bg-cover bg-center bg-repeat" >
           <div className="container mx-auto">
             <NavBar />
             <div className="mt-10" >
@@ -27,6 +27,27 @@ const MyApp: AppType<{ session: Session | null }> = ({
             </div>
           </div>
         </div>
+        <footer className="md:flex md:items-center md:justify-between md:p-6">
+          <ul className="flex flex-wrap items-center mt-3 sm:mt-0">
+            <li>
+              <a
+                href="https://github.com/abdigaliarsen"
+                className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">
+                Created by Arsen Abdigali
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/abdigaliarsen/chess-online"
+                className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">
+                GitHub
+              </a>
+            </li>
+          </ul>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © {new Date().getFullYear()}
+          </span>
+        </footer>
       </div>
     </SessionProvider>
   );
