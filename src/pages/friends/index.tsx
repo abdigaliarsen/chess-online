0e7d942a788449ef4647de866eac1796a0b9b259
quadropunk <abdigal.2015@gmail.com>
2023-05-21 11:38:12 +0600

    added recommendated users

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -2,8 +2,10 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import { faGamepad, faMessage, faUserXmark } from "@fortawesome/free-solid-svg-icons"
+import { faGamepad, faMessage, faUserXmark, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { User } from "@prisma/client";
 
 const FriendsWrapper = () => {
   const session = useSession();
@@ -18,43 +20,119 @@ const Friends = () => {
   if (session.status !== "authenticated")
     return <></>;
 
-  // const { data: friends } = api.user.getFriendsById.useQuery({ id: session.data.user.id }, {
-  //   onError: (error) => {
-  //     console.error(error)
-  //   }
-  // });
+  const [friends, setFriends] = useState<User[]>([]);
+  const [recommendations, setRecommendations] = useState<User[]>([]);
 
-  const friend = {
-    id: "clhqk1fbs0000squ0wnvy115y",
-    name: "Arsen",
-    src: "https://cdn.discordapp.com/avatars/519563633542561802/14afc6fa2c1f0f7d9597102363ae7a90.png",
-    online: "Online"
-  };
+  api.user.getFriendsById.useQuery({ id: session.data.user.id }, {
+    onError: (error) => {
+      console.error(error)
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      setFriends(data);
+    }
+  });
 
-  const friends = [friend, friend, friend, friend, friend, friend, friend, friend];
+  api.user.getRandomUsers.useQuery({ id: session.data.user.id, count: 10 }, {
+    onError: (error) => {
+      console.error(error)
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      setRecommendations(data);
+    }
+  });
 
   return (
     <div className="text-white container mx-auto px-96">
-      <div >
-        <input className="border-solid bg-transparent outline-gray-600 border-gray-600 rounded-m" />
+      <div className="flex justify-center relative">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="pl-2 italic border border-gray-300 w-full bg-transparent rounded" />
+        <FontAwesomeIcon
+          tabIndex={-1}
+          onClick={() => { }}
+          onKeyDown={(e) => { console.log(e) }}
+          onChange={() => { }}
+          className="cursor-pointer absolute right-2 top-1"
+          icon={faMagnifyingGlass} />
       </div>
       <div>
-        {friends?.map((friend) =>
-          <div key={friend.name} className="flex mt-10 justify-between">
+        <div className="flex items-end">
+          <h1 className="text-2xl font-bold mt-10 mr-4">Friends</h1>
+          <span className="h-7 px-2 py-1 text-sm font-semibold bg-gray-500 text-white rounded-lg">
+            {friends?.length}
+          </span>
+        </div>
+        {friends?.map((friend, index) =>
+          <div key={index} className="flex mt-10 justify-between">
             <div className="flex">
               <div className="">
                 <Image
-                  src={friend.src}
+                  src={friend.image ? friend.image : "/default-profile.png"}
                   width={50}
                   height={50}
                   alt="profile picture"
+                  priority={true}
                 />
               </div>
               <div className="flex-col items-center ml-6">
                 <Link href={`/profile/${friend.id}`} className="hover:text-black font-bold">
                   {friend.name}
                 </Link>
-                <p className="text-gray-500">{friend.online}</p>
+                <p className="text-gray-500">{ }</p>
+              </div>
+            </div>
+            <ul className="flex">
+              <li className="ml-4 hover:text-black">
+                <Link
+                  href={{
+                    pathname: "play",
+                    query: { oponent: friend.id }
+                  }}
+                  title="Play">
+                  <FontAwesomeIcon icon={faGamepad} />
+                </Link>
+              </li>
+              <li className="ml-4 hover:text-black">
+                <button title="Message">
+                  <FontAwesomeIcon icon={faMessage} />
+                </button>
+              </li>
+              <li className="ml-4 hover:text-black">
+                <button title="Delete friend">
+                  <FontAwesomeIcon icon={faUserXmark} />
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <div>
+        <div className="flex items-end">
+          <h1 className="text-2xl font-bold mt-10 mr-4">Recommendations</h1>
+          <span className="h-7 px-2 py-1 text-sm font-semibold bg-gray-500 text-white rounded-lg">
+            {recommendations?.length}
+          </span>
+        </div>
+        {recommendations?.map((friend, index) =>
+          <div key={index} className="flex mt-10 justify-between">
+            <div className="flex">
+              <div className="">
+                <Image
+                  src={friend.image ? friend.image : "/default-profile.png"}
+                  width={50}
+                  height={50}
+                  alt="profile picture"
+                  priority={true}
+                />
+              </div>
+              <div className="flex-col items-center ml-6">
+                <Link href={`/profile/${friend.id}`} className="hover:text-black font-bold">
+                  {friend.name}
+                </Link>
+                <p className="text-gray-500">{ }</p>
               </div>
             </div>
             <ul className="flex">
