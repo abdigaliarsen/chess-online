94574dc72e75a438c35ae56a5ba5b0c23ec249c8
quadropunk <abdigal.2015@gmail.com>
2023-06-10 16:31:34 +0600

    added chatting functionality in the friends page

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,10 +1,12 @@
 import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-import { User } from "@prisma/client";
+import { useEffect, useState } from "react";
+import { Messages, User } from "@prisma/client";
 import { UsersList } from "~/widgets/UsersList/UsersList";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { ChatBox } from "~/widgets/Chat/ChatBox";
+import { createMessagesConnection } from "~/features/Messages/createConnection";
 
 const FriendsWrapper = () => {
   const session = useSession();
@@ -19,8 +21,40 @@ const Friends = () => {
   if (session.status !== "authenticated")
     return <></>;
 
+  // user id with whom we are chatting
+  const [activeChatUser, setActiveChatUser] = useState<User | null>(null);
+  const [activeChatMessages, setActiveChatMessages] = useState<Messages[]>([]);
+
+  const [usersMessages, setUsersMessages] = useState<{ [userId: string]: Messages[] } | null>(null);
+
   const [friends, setFriends] = useState<User[]>([]);
   const [recommendations, setRecommendations] = useState<User[]>([]);
+  const [hideChat, setHideChat] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (session.data) {
+      return createMessagesConnection({
+        senderId: session.data.user.id as string,
+        setMessages: setActiveChatMessages,
+      });
+    }
+  }, [session.data?.user.id]);
+
+  useEffect(() => {
+    if (activeChatUser && (!usersMessages || usersMessages[activeChatUser.id])) {
+      setUsersMessages((prev) => {
+        if (prev) {
+          return {
+            ...prev,
+            [activeChatUser.id]: activeChatMessages
+          }
+        }
+        return {
+          [activeChatUser.id]: activeChatMessages
+        }
+      });
+    }
+  }, [activeChatUser, activeChatMessages]);
 
   api.user.getFriendsById.useQuery({ id: session.data.user.id }, {
     onError: (error) => {
@@ -28,6 +62,15 @@ const Friends = () => {
     },
     onSuccess: (data) => {
       setFriends(data);
+    }
+  });
+
+  api.message.getAllMessages.useQuery({ senderId: session.data.user.id }, {
+    onError: (error) => {
+      console.error(error);
+    },
+    onSuccess: (data) => {
+      setUsersMessages(data);
     }
   });
 
@@ -86,24 +129,38 @@ const Friends = () => {
           className="cursor-pointer absolute right-2 top-3"
           icon={faMagnifyingGlass} />
       </div>
-      <div>
-        <div className="flex items-end">
-          <h1 className="text-2xl font-bold mt-10 mr-4">Friends</h1>
-          <span className="h-7 px-2 py-1 text-sm font-semibold bg-gray-500 text-white rounded-lg">
-            {friends?.length}
-          </span>
-        </div>
-        <UsersList id={session.data.user.id} users={friends} unfollow={unfollow} />
-      </div>
-      <div>
-        <div className="flex items-end">
-          <h1 className="text-2xl font-bold mt-10 mr-4">Recommendations</h1>
-          <span className="h-7 px-2 py-1 text-sm font-semibold bg-gray-500 text-white rounded-lg">
-            {recommendations?.length}
-          </span>
-        </div>
-        <UsersList id={session.data.user.id} users={recommendations} follow={follow} />
-      </div>
+      {
+        [
+          { users: friends, title: "Friends" },
+          { users: recommendations, title: "Recommendations" }
+        ].map((item) =>
+          <div>
+            <div className="flex items-end">
+              <h1 className="text-2xl font-bold mt-10 mr-4">{item.title}</h1>
+              <span className="h-7 px-2 py-1 text-sm font-semibold bg-gray-500 text-white rounded-lg">
+                {item.users?.length}
+              </span>
+            </div>
+            <UsersList
+              id={session.data.user.id}
+              users={item.users}
+              type={item.title.toLowerCase() as "friends" | "recommendations"}
+              follow={follow}
+              unfollow={unfollow}
+              setActiveChatUser={setActiveChatUser}
+              setHideChat={setHideChat}
+            />
+          </div>
+        )
+      }
+      {activeChatUser !== null && usersMessages && <ChatBox
+        hidden={hideChat || activeChatUser === null}
+        setHide={setHideChat}
+        sender={session.data.user as User}
+        receiver={activeChatUser}
+        messages={activeChatMessages}
+        setMessages={setActiveChatMessages}
+      />}
     </div>
   )
 };
